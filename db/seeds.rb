1a026224d021fb10e214ff91439f4efbf67ba339
Isabela Santos <isabelatravaglia@gmail.com>
2019-08-26 19:26:22 +0100

    seed file done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.destroy_all
+User.destroy_all
+
+admin = User.create(email:"admin@shared_shelf.com", password:"lorem123456")
+LANGUAGES = ['English', 'Portuguese', 'French', 'German', 'Italian']
+
+puts "Start seeding books..."
+50.times do
+ puts "."
+ book = Book.create(name: Faker::Book.title, author: Faker::Book.author, number_of_pages: rand(50..650), publishing_year: rand(1800..2019), description: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4), user: admin, photo:"wwww.shared_shelf", language: LANGUAGES.sample)
+end
+puts "Done!"
