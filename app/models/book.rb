37ba41586ffa7c062afe369615d1e23b166f87f2
Isabela Santos <isabelatravaglia@gmail.com>
2019-08-26 16:52:05 +0100

    added photo to books, added validations, added relationship between the models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :author, presence: true
+  validates :language, presence: true
+  validates :number_of_pages, presence: true
+  validates :publishing_year, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
 end
