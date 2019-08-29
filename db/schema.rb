be77ddddc78eef1e33f50ab074b82408ea67f893
Isabela Santos <isabelatravaglia@gmail.com>
2019-08-29 19:16:20 +0100

    added status to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_153330) do
+ActiveRecord::Schema.define(version: 2019_08_29_181432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_153330) do
     t.datetime "updated_at", null: false
     t.date "borrow_date"
     t.date "return_date"
+    t.string "status"
     t.index ["book_id"], name: "index_transactions_on_book_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
