58857cc87258aa67662c68987696505dc49d27a5
Veronica Santos <verosbraga@gmail.com>
2019-08-29 17:18:34 +0100

    Changed edition and genres in the files and changed bold end italic borrowed book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -59,6 +59,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:name, :description, :author, :photo, :number_of_pages, :language, :edition)
+    params.require(:book).permit(:name, :description, :author, :photo, :number_of_pages, :language, :edition, :genre)
   end
 end
